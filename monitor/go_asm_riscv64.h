6ff816c1fdd38debbafdf6ad52b8350a2cf42ab6
Andrea Barisani <andrea@inversepath.com>
2022-09-05 10:18:05 +0200

    adjust license

---
+++ monitor/go_asm_riscv64.h
@@ -0,0 +1,23 @@
+// RISC-V processor support
+// https://github.com/usbarmory/tamago
+//
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in Go LICENSE file.
+
+#define t0 5
+#define t1 6
+
+#define satp     0x180
+#define mstatus  0x300
+#define mscratch 0x340
+#define mepc     0x341
+
+#define CSRW(RS,CSR) WORD $(0x1073 + RS<<15 + CSR<<20)
+#define CSRR(CSR,RD) WORD $(0x2073 + RD<<7 + CSR<<20)
+#define CSRS(RS,CSR) WORD $(0x2073 + RS<<15 + CSR<<20)
+#define CSRC(RS,CSR) WORD $(0x3073 + RS<<15 + CSR<<20)
+#define CSRRW(RS,CSR,RD) WORD $(0x1073 + RD<<7 + RS<<15 + CSR<<20)
+
+#define WFI   WORD $0x10500073
+#define MRET  WORD $0x30200073
