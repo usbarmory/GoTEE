7ea78738897b65c3d85b4ead8eee6d278866ee63
Andrea Barisani <andrea@inversepath.com>
2024-12-04 13:11:10 +0100

    bump dependencies

---
+++ applet/applet_arm.go
@@ -0,0 +1,10 @@
+// Copyright (c) WithSecure Corporation
+// https://foundry.withsecure.com
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+package applet
+
+// required to link cpuinit
+import _ "github.com/usbarmory/tamago/arm"
